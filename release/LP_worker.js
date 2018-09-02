495e8f2a27dd632c34a115ad06af6c981265cfa3
J <smaragdinesapphire@google.com>
2018-09-02 13:23:34 +0800

    for debug

--- release/LP_worker.js
+++ release/LP_worker.js
@@ -3,10 +3,16 @@
 
     if (e.data.isWebSite) {
         //web side
+        //if (typeof LP_interface === 'undefined') {
+        //    importScripts("/FGO/release/LP_interface/LPdefs.min.js" + e.data.ver);
+        //    importScripts("/FGO/release/LP_interface/LPmethods.min.js" + e.data.ver);
+        //    importScripts("/FGO/release/LP_interface.min.js" + e.data.ver);
+        //    //JIE.isDebug = true;
+        //}
         if (typeof LP_interface === 'undefined') {
-            importScripts("/FGO/release/LP_interface/LPdefs.min.js" + e.data.ver);
-            importScripts("/FGO/release/LP_interface/LPmethods.min.js" + e.data.ver);
-            importScripts("/FGO/release/LP_interface.min.js" + e.data.ver);
+            importScripts("/FGO/release/LP_interface/LPdefs.js" + e.data.ver);
+            importScripts("/FGO/release/LP_interface/LPmethods.js" + e.data.ver);
+            importScripts("/FGO/release/LP_interface.js" + e.data.ver);
             //JIE.isDebug = true;
         }
     }
